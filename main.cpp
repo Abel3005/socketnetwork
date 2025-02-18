40248862037aa852f82ea23de27b90c7a87da7c4
Abel3005 <kimdaegun1114@gmail.com>
2025-02-18 23:18:19 +0900

    start socket but cmake build error

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <winsock2.h>
+#pragma comment(lib,"libws2_32.a")
+using namespace std;
+
+#define PORT 5500
+#define PACKET_SIZE 1024
 
 int main(int, char**){
-    std::cout << "Hello, from socketnetwork!\n";
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2,2), &wsaData);
+    WSACleanup();
 }
